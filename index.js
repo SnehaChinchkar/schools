6021893e80d5c2a99ec033561b115844e4617a7f
SnehaChinchkar <131424478+SnehaChinchkar@users.noreply.github.com>
2025-03-07 23:47:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password:'',
     database: process.env.DB_NAME
 });
 
